d97411f8b48590be2f040327f7bab5ff83718086
BuniBoi <116119313+BuniBoi@users.noreply.github.com>
2023-03-07 09:27:10 -0800

    s

--- rat-crusher/Assets/Scripts/Score.cs
+++ rat-crusher/Assets/Scripts/Score.cs
@@ -7,21 +7,51 @@ public class Score : MonoBehaviour
 {
 
     public TextMeshProUGUI timeText;
-    public GameObject GameOver;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI finalScoreText;
+    public TextMeshProUGUI highScoreText;
+    public GameObject player;
+    public GameObject gameOver;
     private float time = 5f;
-
+    private int score;
+    private int highScore;
 
     void Update()
     {
-        if(time >= 0)
+        Timer();
+        Scored();
+    }
+
+    private void Timer()
+    {
+        if (time >= 0)
         {
             time -= Time.deltaTime;
             timeText.text = Mathf.Round(time) + "s";
         }
-        else if(time < 0)
+        else if (time < 0)
         {
             Time.timeScale = 0;
-            GameOver.SetActive(true);
+            gameOver.SetActive(true);
+            finalScoreText.text = "Score: " + score;
+            highScoreText.text = "Highscore: " + highScore;
+        }
+    }
+
+    private void Scored()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            highScore = PlayerPrefs.GetInt("HighScore");
+        }
+        else
+        {
+            highScore = 0;
+        }
+        //score = player.GetComponent<PlayerMove>().score;
+        if(score >= highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
         }
     }
 }
