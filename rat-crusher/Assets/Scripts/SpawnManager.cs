cf648e994e68d6c1f291e8d8f8a06e3df2aa3374
BuniBoi <116119313+BuniBoi@users.noreply.github.com>
2023-03-07 10:35:42 -0800

    :333

--- rat-crusher/Assets/Scripts/SpawnManager.cs
+++ rat-crusher/Assets/Scripts/SpawnManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+
+    public float Rats;
+    public GameObject ratPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Rats = GameObject.FindGameObjectsWithTag("rat").Length;
+        if(Rats <= 1)
+        {
+            Instantiate(ratPrefab, new Vector3(Random.Range(-15, 15), -6, 0), Quaternion.identity);
+        }
     }
 }
--- rat-crusher/Assets/scripts/ratController.cs
+++ rat-crusher/Assets/scripts/ratController.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class ratController : MonoBehaviour
 {
     Rigidbody2D rb;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void die()
     {
-        Debug.Log("rat ded");
+        player = GameObject.FindGameObjectWithTag("Player");
         Destroy(gameObject);
+        player.GetComponent<Score>().score += 1;
     }
 }
