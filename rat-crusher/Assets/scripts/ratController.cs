b9b21b456cb2194257a32591204966b6a2c0aaf4
BuniBoi <116119313+BuniBoi@users.noreply.github.com>
2023-03-07 10:01:20 -0800

    wsasedrtfyguhjikol

--- rat-crusher/Assets/scripts/ratController.cs
+++ rat-crusher/Assets/scripts/ratController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ratController : MonoBehaviour
 {
+
+    private GameObject mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
@@ -18,5 +21,6 @@ public class ratController : MonoBehaviour
     public void die()
     {
         Destroy(gameObject);
+        mainCamera.GetComponent<Score>().score += 1;
     }
 }
