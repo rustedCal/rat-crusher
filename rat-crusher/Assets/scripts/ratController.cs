40da00c5a37343cdb13f05a318538b0947bcf6dd
cooljam206 <4017157@g.highlineschools.org>
2023-03-08 08:29:48 -0800

    mvh f jcgfcg jhfcg jfcg jfg jgf jfcg jfjcgfbcghybrftcubfytytfibytfiybytccfhktfgjmbryf uilhygyubo

--- rat-crusher/Assets/scripts/ratController.cs
+++ rat-crusher/Assets/scripts/ratController.cs
@@ -4,24 +4,70 @@ using UnityEngine;
 
 public class ratController : MonoBehaviour
 {
+    public float speed = 5.0f;
     Rigidbody2D rb;
+<<<<<<< Updated upstream
     public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
 
+=======
+    public AudioClip death;
+    AudioSource aud;
+    Animator anim;
+    bool dead = false;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        aud = gameObject.GetComponent<AudioSource>();
+        anim = gameObject.GetComponent<Animator>();
+        //get player pos for look dir in anim, if < rat pos then 0, else 1
+>>>>>>> Stashed changes
     }
-
-    // Update is called once per frame
     void Update()
     {
 
+<<<<<<< Updated upstream
     }
     public void die()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         Destroy(gameObject);
         player.GetComponent<Score>().score += 1;
+=======
+
+        //animation
+        anim.SetBool("dead", dead);
+        //killing
+        if (dead && !aud.isPlaying)//destroy when audio is done playing and dead
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void die()
+    {
+        aud.PlayOneShot(death);
+        Debug.Log("rat ded");
+        dead = true;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        player play = collision.GetComponent<player>();
+        if (play != null)
+        {
+            if (play.transform.position.x < gameObject.transform.position.x)
+            {
+                anim.SetFloat("lookDir", 0);
+                rb.velocity.Set(speed, 0.0f);
+            }
+            else
+            {
+                anim.SetFloat("lookDir", 1);
+                rb.velocity.Set(-speed, 0.0f);
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
